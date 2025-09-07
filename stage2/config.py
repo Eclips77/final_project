6a3591bcf57f1ef60d3bca5555ff55408d8861bb
Eclipse <Eclips77@users.noreply.github.com>
2025-09-07 16:05:17 +0300

    add variables into config and update requirements

--- stage2/config.py
+++ stage2/config.py
@@ -2,7 +2,7 @@ import os
 
 # a variable to get the audio files path
 FILES_PATH = os.getenv("FILES_PATH","C:/Users/brdwn/Desktop/my_projects/final_proj_data")
-
 # a variable to get the mongo connection info
 MONGO_URI = os.getenv("MONGO_URI","")
-
+# a variable to get the elastic index
+ES_INDEX = os.getenv("ES_INDEX","")
---
+++ stage2/src/consumer.py
@@ -0,0 +1,4 @@
+from kafka import KafkaConsumer
+import logging
+
+logger = logging.getLogger(__name__)
