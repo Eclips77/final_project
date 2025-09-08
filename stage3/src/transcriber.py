4b0cf11dac5d66c19c7c04c959b3656e3d39e87c
Eclipse <Eclips77@users.noreply.github.com>
2025-09-08 17:38:51 +0300

    Add logging to transcriber

--- stage3/src/transcriber.py
+++ stage3/src/transcriber.py
@@ -1,9 +1,15 @@
 import speech_recognition as sr
 from ..utils import config
+# import os
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class SpeechToTextConverter:
     def __init__(self):
         self.recognizer = sr.Recognizer()
+        self.pathes = self.read_file_paths(config.FILES_PATH)
 
     def transcribe_audio_file(self, audio_file_path):
         """
@@ -19,24 +25,38 @@ class SpeechToTextConverter:
             with sr.AudioFile(audio_file_path) as source:
                 audio_data = self.recognizer.record(source)
                 text = self.recognizer.recognize_google(audio_data)
+                logger.info(f"audio file converted to text.")
                 return text
         except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio.")
+            logger.error("Google Speech Recognition could not understand audio.")
             return None
         except sr.RequestError as e:
-            print(f"Could not request results from Google Speech Recognition service; {e}")
+            logger.error(f"Could not request results from Google Speech Recognition service; {e}")
             return None
         except FileNotFoundError:
-            print(f"Audio file not found at: {audio_file_path}")
+            logger.error(f"Audio file not found at: {audio_file_path}")
             return None
 
+#     @staticmethod
+#     def read_file_paths(directory:str)->list[str]:
+#         """
+#         a method that returns all the file pathes in a directory
 
+#         Args:
 
-if __name__ == "__main__":
-    converter = SpeechToTextConverter()
-    for path in config.FILES_PATH:
-        transcribed_text_file = converter.transcribe_audio_file(config.FILES_PATH)
-        if transcribed_text_file:
-            print(f"Transcribed from file: {transcribed_text_file}")
-        break
+#             directory path str.
+#         Returns:
+        
+#                 all the files pathes in the directory.
+#         """
+#         return [os.path.join(directory, file) for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
+
+#  if __name__ == "__main__":
+#     converter = SpeechToTextConverter()
+#     path = converter.pathes[5]
+#     x = converter.transcribe_audio_file(path)
+#     if x:
+#         print(f"converted data {x}")
+
+      
 
--- stage3/utils/config.py
+++ stage3/utils/config.py
@@ -4,3 +4,4 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..","..","..
 DATA_DIR = os.path.join(BASE_DIR, "final_proj_data")
 
 FILES_PATH : str = os.getenv("FILES_PATH",DATA_DIR)
+
