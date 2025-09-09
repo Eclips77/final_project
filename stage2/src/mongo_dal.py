7bce48376cba2c21ff7a9d2f4f969203b0621052
Eclipse <Eclips77@users.noreply.github.com>
2025-09-09 10:29:17 +0300

    Refactor MongoStore methods: change file_id type from int to str in exists and save_file methods

--- stage2/src/mongo_dal.py
+++ stage2/src/mongo_dal.py
@@ -14,11 +14,11 @@ class MongoStore:
         self.db = self.client[db_name]
         self.fs = gridfs.GridFS(self.db)
 
-    def exists(self, file_id: int) -> bool:
+    def exists(self, file_id: str) -> bool:
         """Return True if a file with the given ID exists in GridFS."""
         return self.db.fs.files.find_one({"_id": file_id}) is not None
 
-    def save_file(self, file_id: int, file_path: str) -> int:
+    def save_file(self, file_id: str, file_path: str) -> str:
         """
         save a file into the db.
 
