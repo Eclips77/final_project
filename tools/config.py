0a0bd419356816c3a6991d3915b06e3ef23959de
Eclipse <Eclips77@users.noreply.github.com>
2025-09-10 16:10:07 +0300

    Update docker-compose and config for service integration and environment variables

--- tools/config.py
+++ tools/config.py
@@ -2,10 +2,10 @@ import os
 from typing import List
 from pathlib import Path
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..","..","..")) # i removed one ".."
-DATA_DIR = os.path.join(BASE_DIR, "final_proj_data")
+# BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..","..","..")) # i removed one ".."
+# DATA_DIR = os.path.join(BASE_DIR, "final_proj_data")
 
-DATA_DIR_2 = Path("C:/Users/brdwn/Desktop/my_projects/final_proj_data")
+DATA_DIR = os.getenv("DATA_DIR","C:/Users/brdwn/Desktop/my_projects/final_proj_data")
 
 
 # a variable to get the audio files path
